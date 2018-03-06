956f2cc6a66cb3d18bc5ed091c26613fca3a1afc
mapomatic <mapomatic.waze@gmail.com>
2018-03-06 11:38:26 -0500

    Update GoogleLinkEnhancer.js
    version bump

--- GoogleLinkEnhancer.js
+++ GoogleLinkEnhancer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Utils - Google Link Enhancer
 // @namespace    WazeDev
-// @version      2018.03.05.001
+// @version      2018.03.06.001
 // @description  Adds some extra WME functionality related to Google place links.
 // @author       MapOMatic, WazeDev group
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
